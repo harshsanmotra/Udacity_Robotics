c07f90af0dde212e1964706ac50e4270325498f4
harshsanmotra <sanmotra.harsh@gmail.com>
2024-06-25 04:05:21 -0400

    Made Corrections

--- Project_1/build/CMakeFiles/Makefile.cmake
+++ Project_1/build/CMakeFiles/Makefile.cmake
@@ -257,5 +257,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/helloo.dir/DependInfo.cmake"
+  "CMakeFiles/welcome_msg.dir/DependInfo.cmake"
   )
--- Project_1/script/welcome_msg.cpp
+++ Project_1/script/welcome_msg.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Welcome to this World!\n");
+              printf("Welcome to Harsh's World!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
